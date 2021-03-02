806551ba4a04ba60ed54555d1b4a86ceb6acc91a
r47717 <mchr@yandex.ru>
2021-03-02 15:50:36 +0300

    export component missed

---
+++ src/components/Export.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <h1>Export/Import Activities & Tasks</h1>
+    <v-sheet class="pa-5">
+      <v-card class="pa-5">
+        <v-textarea outlined rows="15" label="Data"></v-textarea>
+        <div class="d-flex">
+          <v-btn>
+            <v-icon left>mdi-download</v-icon>
+            <span>Generate</span>
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn>
+            <v-icon left>mdi-upload</v-icon>
+            <span>Upload</span>
+          </v-btn>
+        </div>
+      </v-card>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
